e0b917c64acda7bb7786ccda8b054985f131ac0a
robotprobot <swheeler2013@outlook.com>
2022-01-22 20:17:32 +0000

    OTA is now handled in ota.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,20 @@
-#include <Arduino.h>
-#include <Preferences.h>
-#include <WiFi.h>
-#include <ESPmDNS.h>
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
-#include "remotesetup.h"
+#include "main.h"
+#include "unitidentifier.h"
 
-const char* setup_ap_ssid = "QuestEyes-Setup-000000";
-const char* setup_ap_pass = "questeyes";
+OV2640 cam;
 
 // declare pins on the board
 void declarePins() {
 
-  //TODO: DECLARE PINS
-  
 }
 
 // setup runs one time when reset is pressed or the board is powered
 void setup() {
   declarePins();
   //prepare storage to check for wifi credentials
+
+  //TODO: SETUP LED WHITE WHILE BOOTING
+
   Preferences storage;
   storage.begin("settings", false);
 
@@ -40,52 +35,38 @@ void setup() {
     int timeout = 0;
     while(WiFi.status() != WL_CONNECTED && timeout < 30000){
 
-      //TODO: SETUP LED FLASHING PULSING WHILE TRYING TO CONNECT
+      //TODO: SETUP LED FLASHING PULSING WHITE WHILE TRYING TO CONNECT
 
       delay(100);
       timeout += 100;
+      //print a dot every 500ms
+      if(timeout % 500 == 0){
+        Serial.print(".");
+      }
     }
     //if connected, print out the ip address and continue the bootup process
     if(WiFi.status() == WL_CONNECTED){
       Serial.println("WiFi connected successfully.");
       Serial.println("IP address: " + String(WiFi.localIP()));
 
-      //TODO: SETUP LED GOING GREEN AFTER A SUCCESSFUL CONNECTION
+      //TODO: SETUP LED FLASHING WHITE AND GREEN AFTER A SUCCESSFUL CONNECTION
 
       //activate the OTA system
       Serial.println("Initializing OTA system...");
-      ArduinoOTA
-        .onStart([]() {
-            String type;
-            
-            if (ArduinoOTA.getCommand() == U_FLASH)
-                type = "sketch";
-            else
-                type = "filesystem";
+      startOTA("QuestEyes-" + unit_identifier);
 
-            Serial.println("OTA start updating " + type);
-        })
-        .onEnd([]() {
-            Serial.println("\nOTA finished. System rebooting...");
-            ESP.restart();
-        })
-        .onProgress([](unsigned int progress, unsigned int total) {
-            Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-        })
-        .onError([](ota_error_t error) {
-            Serial.printf("Error[%u]: ", error);
-            if (error == OTA_AUTH_ERROR) Serial.println("OTA authentication Failed");
-            else if (error == OTA_BEGIN_ERROR) Serial.println("OTA failed to start");
-            else if (error == OTA_CONNECT_ERROR) Serial.println("OTA failed to connect");
-            else if (error == OTA_RECEIVE_ERROR) Serial.println("OTA failed to recieve");
-            else if (error == OTA_END_ERROR) Serial.println("OTA failed to end");
-        });
-        ArduinoOTA.begin();
-        Serial.println("OTA system ready to receive.");
+      //prepare the camera
+      delay(100);
+      cam.init(esp32cam_aithinker_config);
+      delay(100);
+      Serial.println("Initializing camera...");
+      Serial.print("RTSP server: rtsp://");
+      Serial.print(WiFi.localIP());
+      Serial.println(":8554/mjpeg/1\n");
+	    initRTSP();
 
-      //TODO: SETUP CAMERA SYSTEM READY TO STREAM
+      //TODO: SETUP LED FLASHING GREEN FOR 2 SECONDS AFTER A SUCCESSFUL CAMERA SYSTEM INIT
 
-      return;
     }
     //if failed to connect, start remote setup.
     else {
@@ -93,24 +74,28 @@ void setup() {
 
       //TODO: SETUP LED FLASHING ORANGE AFTER A FAILED CONNECTION
 
-      remoteSetup(setup_ap_ssid, setup_ap_pass);
+      remoteSetup("QuestEyes-" + unit_identifier);
     }
   }
   //if ssid and password are empty, start remote setup.
   else {
     Serial.println("No Wifi credentials found - starting remote setup...");
 
-    //TODO: SETUP LED FLASHING ORANGE
+    //TODO: SETUP LED FLASHING ORANGE AFTER NO WIFI CREDENTIALS
 
-    remoteSetup(setup_ap_ssid, setup_ap_pass);
+    remoteSetup("QuestEyes-" + unit_identifier);
   }
+
+  //TODO: SETUP LED TURNING OFF AFTER A SUCCESSFUL BOOTUP
+  
+  Serial.println("System is ready for use.");
 }
 
 // main loop
 void loop() {
   //call the OTA system
-  ArduinoOTA.handle();
+	ArduinoOTA.handle();
 
-  //TODO: SETUP CAMERA STREAM TO SERVER
-  
+  //loop
+  delay(100);
 }
